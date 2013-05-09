18c3503ba526f28789b26d4898a811d59c420016
clarencemartens <clarence@ballistiq.com>
2013-05-09 16:19:11 -0700

    rename

--- android/theLife/src/com/p2c/thelife/HelpNewUserActivity.java
+++ android/theLife/src/com/p2c/thelife/HelpNewUserActivity.java
@@ -12,18 +12,18 @@ import android.view.View;
  * @author clarence
  *
  */
-public class NewUserHelpActivity extends Activity {
+public class HelpNewUserActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_new_user_help);
+		setContentView(R.layout.activity_help_new_user);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.new_user_help, menu);
+		getMenuInflater().inflate(R.menu.help_new_user, menu);
 		return true;
 	}
 	
