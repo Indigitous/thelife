56b98607bde6fd24297b5f3a02922a64e49a2de8
clarencemartens <clarence@ballistiq.com>
2013-07-31 10:40:16 -0700

    rename classes for clarity

--- android/theLife/src/com/p2c/thelife/GroupActivity.java
+++ android/theLife/src/com/p2c/thelife/GroupActivity.java
@@ -120,7 +120,7 @@ public class GroupActivity extends SlidingMenuPollingFragmentActivity implements
 			startActivity(intent);
 		} else if (item.getItemId() == R.id.action_new) {
 			if (m_group != null) {
-				GroupInvitationManuallyDialog dialog = new GroupInvitationManuallyDialog();
+				GroupInviteManuallyDialog dialog = new GroupInviteManuallyDialog();
 				dialog.show(getSupportFragmentManager(), dialog.getClass().getSimpleName());
 			}
 		}  else if (item.getItemId() == android.R.id.home) {
